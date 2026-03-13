bb0692b1b4cf33633519a59a916343b9c85457f7
Mehaboob <sameernaseema175@gmail.com>
2026-03-13 23:14:03 +0530

    first-commit

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
 
   name: String,
-  email: { type: String,  },
+  email: { type: String,unique: true },
   password: String,
   phone: String,
   city: String,
